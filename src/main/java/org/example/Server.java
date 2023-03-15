dfd4c1695c275eb7fdfccc33f037ce3f6f53208e
smelkin_nikita <nik24sml2000@gmail.com>
2023-03-15 15:31:12 +0300

    добавлена реализация шифрования RSA для безопасного обмена данными

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -2,9 +2,18 @@ package org.example;
 
 import org.json.JSONObject;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.sql.*;
+import java.util.Base64;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -17,17 +26,27 @@ class ServerSomthing extends Thread {
     // кроме него - клиент и сервер никак не связаны
     private BufferedReader in; // поток чтения из сокета
     private BufferedWriter out; // поток завписи в сокет
-
     private Operations_List oper;
+    PublicKey publicKey = null;
+    PublicKey publicKey_client = null;
+    PrivateKey privateKey = null;
 
 
-    public ServerSomthing(Socket socket) throws IOException {
+    public ServerSomthing(Socket socket) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         this.socket = socket;
 
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
         System.out.println("установлено новое соединение");
+
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        KeyPair pair = generator.generateKeyPair();
+
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+        this.read_key();
+        this.send_key();
 
         start(); // вызываем run()
     }
@@ -36,23 +55,33 @@ class ServerSomthing extends Thread {
     @Override
     public void run() {
         String input_string;
+
         try {
             while (true) {
                 input_string = in.readLine();
+                System.out.println(input_string);
+                Cipher decryptCipher = Cipher.getInstance("RSA");
+                decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
 
-                JSONObject json = new JSONObject(input_string);
+                byte[] decryptedMessageBytes = decryptCipher.doFinal(Base64.getDecoder().decode(input_string));
+                String decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
+
+                JSONObject json = new JSONObject(decryptedMessage);
 
                 if(json.getString("OPERATION").equals("-1")) {
                     this.downService(); // харакири
                     break; // если пришла пустая строка - выходим из цикла прослушки
                 }
 
+
+                System.out.println(decryptedMessage);
+
                 this.oper = new Operations_List(Server.pg_adr,Server.pg_password, json);
-                var result = this.oper.processing(json.getString("OPERATION"));
+                var out_result = this.oper.processing(json.getString("OPERATION"));
 
-                System.out.println(result);
+                System.out.println(out_result);
 
-                this.send(result);
+                this.send(out_result);
 
             }
         } catch (NullPointerException ignored) {} catch (Exception e) {
@@ -61,8 +90,27 @@ class ServerSomthing extends Thread {
         }
     }
 
+
+    private void read_key() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String input_string = in.readLine();
+
+        byte[] data = Base64.getDecoder().decode(input_string.getBytes());
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        this.publicKey_client = fact.generatePublic(spec);
+    }
+
+    private void send_key() {
+        try {
+            out.write(Base64.getEncoder().encodeToString(publicKey.getEncoded()) + "\n");
+            out.flush();
+        } catch (IOException ignored) {}
+
+    }
+
     private void send(String msg) {
         try {
+
             out.write(msg + "\n");
             out.flush();
         } catch (IOException ignored) {}
@@ -121,6 +169,10 @@ public class Server {
                     // Если завершится неудачей, закрывается сокет,
                     // в противном случае, нить закроет его:
                     socket.close();
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                } catch (InvalidKeySpecException e) {
+                    throw new RuntimeException(e);
                 }
             }
         } finally {
